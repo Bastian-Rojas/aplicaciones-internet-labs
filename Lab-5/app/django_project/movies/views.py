1054607d90e6163f40d28c73c3734afe2cfb8989
Bastian Ignacio Rojas Pereira <64908739+Bastian-Rojas@users.noreply.github.com>
2023-12-07 15:28:12 -0300

    Add files via upload

--- Lab-5/app/django_project/movies/views.py
+++ Lab-5/app/django_project/movies/views.py
@@ -6,15 +6,14 @@ from .forms import GenreForm
 
 def index(request):
     api_key = "c4ab6bd505b68822b549cd53ec9fe2bc"
-    url = f"https://api.themoviedb.org/3/movie/now_playing?api_key={api_key}&language=en-US&page=1"
+    url = f"https://api.themoviedb.org/3/movie/now_playing?api_key={api_key}&language=es-ES&page=1"
 
     try:
         response = urllib.request.urlopen(url)
         data = json.loads(response.read().decode())
         movie_list = data["results"]
 
-        # Obtener géneros de películas
-        genre_url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={api_key}&language=en-US"
+        genre_url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={api_key}&language=es-ES"
         genre_response = urllib.request.urlopen(genre_url)
         genre_data = json.loads(genre_response.read().decode())
         genres = [(g['id'], g['name']) for g in genre_data['genres']]
@@ -27,7 +26,7 @@ def index(request):
 def your_ajax_view(request):
     api_key = "c4ab6bd505b68822b549cd53ec9fe2bc"
     genre = request.GET.get('genre')
-    genre_movies_url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}&language=en-US&with_genres={genre}"
+    genre_movies_url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}&language=es-ES&with_genres={genre}"
 
     try:
         response = urllib.request.urlopen(genre_movies_url)
